bf13d9c09c7bd37b786be6a1268bbd30bc749769
Stepan Demchenko <ese4@yandex.ru>
2025-04-03 19:15:29 +0700

    Запуск сервера, прохождение тестов в Postman

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -10,4 +10,7 @@ class OwnerOrReadOnly(permissions.BasePermission):
         )
 
     def has_object_permission(self, request, view, obj):
-        return obj.owner == request.user
+        return (
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+        )
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,25 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
+from .views import PostViewSet, GroupViewSet, CommentViewSet, FollowViewSet
+
+router = DefaultRouter()
+
+router.register('posts', PostViewSet, basename='posts')
+
+router.register('groups', GroupViewSet, basename='groups')
+
+router.register('follow', FollowViewSet, basename='follows')
+
+router.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
 
 urlpatterns = [
+    path('v1/', include(router.urls)),
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
 ]
