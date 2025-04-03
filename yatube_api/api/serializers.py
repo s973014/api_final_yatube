bf50b47f5900e6b768a114c60b93f3bd7e22f47e
Stepan Demchenko <ese4@yandex.ru>
2025-04-03 17:43:31 +0700

    Инициализация сериализаторов (serializers.py)

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
 
-from posts.models import Comment, Post
+from posts.models import Comment, Post, Group, Follow
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -14,10 +14,34 @@ class PostSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
+
     author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
+        read_only = True, 
+        slug_field = 'username'
     )
 
     class Meta:
         fields = '__all__'
         model = Comment
+
+class GroupSerializer(serializers.ModelSerializer):
+ 
+     class Meta:
+         model = Group
+         fields = '__all__'
+
+class FollowSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = ('user', 'following')
+        model = Follow
+
+    user = SlugRelatedField(
+        slug_field = 'username',
+        queryset = User.objects.all(),
+        default = serializers.CurrentUserDefault()
+    )
+    following = serializers.SlugRelatedField(
+        slug_field = 'username', 
+        queryset = User.objects.all()
+    )
